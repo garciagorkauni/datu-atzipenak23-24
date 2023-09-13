e43022cfd99f4f4c782378fd332b94520de70f54
Gorka Garcia <garcia.gorka@uni.eus>
2023-09-13 12:24:06 +0200

    Make CopyCharactersMaiuskulaz

---
+++ 1-Fitxategiak/iostreams/src/main/java/dambi/CopyCharactersMaiuskulaz.java
@@ -0,0 +1,35 @@
+package dambi;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CopyCharactersMaiuskulaz {
+    public static void main(String[] args) throws IOException {
+
+        FileReader inputStream = null;
+        FileWriter outputStream = null;
+
+        try {
+            inputStream = new FileReader("xanadu.txt");
+            outputStream = new FileWriter("characteroutput.txt");
+
+            int c;
+            while ((c = inputStream.read()) != -1) {
+                System.out.println();
+                
+                if(c >= 97 && c <= 122 ){
+                    c -= 32;
+                }
+                outputStream.write(c);
+            }
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        }
+    }
+}
