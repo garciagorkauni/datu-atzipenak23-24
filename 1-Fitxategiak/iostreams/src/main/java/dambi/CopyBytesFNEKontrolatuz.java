3bee63a30f52280b06a7ecb37be8c91392859217
Gorka Garcia <garcia.gorka@uni.eus>
2023-09-13 12:07:06 +0200

    Make CopyBytesFNEKontrolatuz

---
+++ 1-Fitxategiak/iostreams/src/main/java/dambi/CopyBytesFNEKontrolatuz.java
@@ -0,0 +1,39 @@
+package dambi;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CopyBytesFNEKontrolatuz {
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream in = null;
+        FileOutputStream out = null;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sartu kopiatzeko fitxategiaren izena (outagain.txt fitxategian kopiatuko da): ");
+        String input_fitxategia = input.next();
+        input.close();
+        
+        try {
+            in = new FileInputStream(input_fitxategia);
+            out = new FileOutputStream("outagain.txt");
+            int c;
+
+            while ((c = in.read()) != -1) {
+                out.write(c);
+            }
+        } catch (FileNotFoundException e){
+            System.out.println("Fitxategia ez da aurkitu.");
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+}
