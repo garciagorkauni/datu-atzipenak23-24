edd1f45c2aea8aa5ef1fbefeda491bb7d43887c6
Gorka Garcia <garcia.gorka@uni.eus>
2023-10-09 08:48:49 +0200

    Create JsonZuhaitzaSortu.java

---
+++ 3-JSONFitxategiak/jsonfiles/src/main/java/dambi/JsonZuhaitzaSortu.java
@@ -0,0 +1,25 @@
+package main.java.dambi;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+
+public class JsonZuhaitzaSortu {
+    public static void main(String[] args) {
+        JsonObject model = Json.createObjectBuilder()
+            .add("firstName", "Duke")
+            .add("lastName", "Java")
+            .add("age", 18)
+            .add("streetAddress", "100 Internet Dr")
+            .add("city", "JavaTown")
+            .add("state", "JA")
+            .add("postalCode", "12345")
+            .add("phoneNumbers", Json.createArrayBuilder()
+                .add(Json.createObjectBuilder()
+                    .add("type", "mobile")
+                    .add("number", "111-111-1111"))
+                .add(Json.createObjectBuilder()
+                    .add("type", "home")
+                    .add("number", "222-222-2222")))
+            .build();
+    }
+}
