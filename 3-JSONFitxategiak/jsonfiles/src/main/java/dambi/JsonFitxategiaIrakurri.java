a20573402f3776a5b538beb83f8374e2fcb4767a
Gorka Garcia <garcia.gorka@uni.eus>
2023-10-09 08:42:16 +0200

    Create JsonFitxategiaIrakurri

---
+++ 3-JSONFitxategiak/jsonfiles/src/main/java/dambi/JsonFitxategiaIrakurri.java
@@ -0,0 +1,13 @@
+package main.java.dambi;
+
+import java.io.FileReader;
+import javax.json.Json;
+import javax.json.JsonReader;
+import javax.json.JsonStructure;
+
+public class JsonFitxategiaIrakurri {
+    public static void main(String[] args) {
+        JsonReader reader = Json.createReader(new FileReader("jsondata.txt"));
+        JsonStructure jsonst = reader.read();
+    }
+}
